8a3ccdcfde6b5828e49f231de3e1e9568411815f
Ken-S-0114 <g031n071@s.iwate-pu.ac.jp>
2018-03-29 00:13:00 +0900

    RxDatasourseを追加

---
+++ RxSwift_Badge/Model/Badge.swift
@@ -0,0 +1,27 @@
+//
+//  Badge.swift
+//  RxSwift_Badge
+//
+//  Created by 佐藤賢 on 2018/03/29.
+//  Copyright © 2018年 佐藤賢. All rights reserved.
+//
+
+import UIKit
+
+struct Badge {
+  let id: Int
+  let title: String
+  let color: UIColor
+}
+
+// idをもとにハッシュ値を計算可能にする
+extension Badge: Hashable {
+  var hashValue: Int {
+    return self.id
+  }
+  
+  // 同値性を確認するためのプロトコル
+  static func ==(lhs: Badge, rhs: Badge) -> Bool {
+    return lhs.id == rhs.id
+  }
+}
